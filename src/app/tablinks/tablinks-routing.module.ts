bc1c4fa3024100e8f6475bdcf43b5410141a8b30
Sketchead <megamam_atxel@hotmail.com>
2022-10-27 17:59:22 -0600

    Add missing files

---
+++ src/app/tablinks/tablinks-routing.module.ts
@@ -0,0 +1,42 @@
+// tablinks-routing.module.ts
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TablinksPage } from './tablinks.page';
+
+const routes: Routes = [
+  {
+    path: 'tablinks',
+    component: TablinksPage,
+    children: [
+      {
+        path: 'map',
+        loadChildren: () => import('../map/map.module').then(m => m.MapPageModule)
+      },
+      {
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('../profile/profile.module').then(m => m.ProfilePageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/tablinks/home',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tablinks/home',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TablinksPageRoutingModule { }
