86551bd6c33a976cecd72b753109838a7cd08679
Sketchead <megamam_atxel@hotmail.com>
2022-10-26 20:40:38 -0600

    Add Firebase Services

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'geolocal-77b41',
+    appId: '1:676916917919:web:ad6ee3cee710db5dc194aa',
+    storageBucket: 'geolocal-77b41.appspot.com',
+    apiKey: 'AIzaSyBCHlBIIiAWpgUnr-kFR0NEJ79z6DJN5_U',
+    authDomain: 'geolocal-77b41.firebaseapp.com',
+    messagingSenderId: '676916917919',
+    measurementId: 'G-5VN58QJQM2',
+  },
   production: true
 };
