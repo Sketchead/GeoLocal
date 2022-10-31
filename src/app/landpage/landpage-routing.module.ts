1443e159b9ccd7a43fa43af373450a1ac052b4c4
Edsson Tolentino <edssongera@hotmail.com>
2022-10-30 17:55:57 -0700

    Add pages routing

--- src/app/landpage/landpage-routing.module.ts
+++ src/app/landpage/landpage-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePage } from '../home/home.page';
+import { LoginPage } from '../login/login.page';
+import { RegisterClientPage } from '../register-client/register-client.page';
+import { RegisterRestPage } from '../register-rest/register-rest.page';
 
 import { LandpagePage } from './landpage.page';
 
@@ -7,6 +11,24 @@ const routes: Routes = [
   {
     path: '',
     component: LandpagePage
+  },
+  {
+    path: 'home',
+    component: HomePage
+  },
+  {
+    path: 'login',
+    component: LoginPage
+  }
+  ,
+  {
+    path: 'register-client',
+    component: RegisterClientPage
+  }
+  ,
+  {
+    path: 'register-res',
+    component: RegisterRestPage
   }
 ];
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { createUserWithEmailAndPassword, signOut } from '@firebase/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth:Auth) {
+  }
+
+  async register({email,password}){
+    try{
+      const user = await createUserWithEmailAndPassword(this.auth,email,password);
+    }catch(e){
+      return null;
+    }
+  }
+
+  async login ({email,password}){
+
+  }
+
+  logout(){
+    return signOut(this.auth)
+  }
 }
