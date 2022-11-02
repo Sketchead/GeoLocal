21e436099c733395a6ab685f7732d5c9c2af9f45
heleperezcr <heleperezcr@ittepic.edu.mx>
2022-11-02 12:35:49 -0600

    cambios login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +10,76 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  credentials: FormGroup;
 
-  constructor(private router:Router) { }
+  constructor(
+    private fb: FormBuilder, 
+    private router:Router,
+    private alertController: AlertController,
+    private authService:AuthService,
+    private loadingController:LoadingController,) { }
+
+  get email(){
+    return this.credentials.get('email');
+  }
+  
+  get password(){
+    return this.credentials.get('password');
+  }
 
   ngOnInit() {
   }
 
   register(){
       this.router.navigateByUrl('/app/home',{replaceUrl:true});
+  }
+
+  //------------------------GOOGLE----------------------
+  async googleRegister(){
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+    const petition = new Promise((resolve,reject)=>{
+      const user = this.authService.googleregister()
+      console.log(user)
+      if(user){
+        resolve('exito')
+      }else{
+        reject('fallo')
+      }
+    });
+
+    await loading.dismiss();
+
+    petition.then((message)=>{ 
+      this.router.navigateByUrl('/user-type',{replaceUrl:true});
+    }).catch((message)=>{
+      //this.showAlert('No se pudo acceder',message)
+    })
+    
+  }
+  //------------------------FACEBOOK----------------------
+  async facebookRegister(){
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+    const petition = new Promise((resolve,reject)=>{
+      const user = this.authService.facebookregister()
+      console.log(user)
+      if(user){
+        resolve('exito')
+      }else{
+        reject('fallo')
+      }
+    });
+
+    await loading.dismiss();
+
+    petition.then((message)=>{ 
+    }).catch((message)=>{
+      //this.showAlert('No se pudo acceder',message)
+    })
+    await this.router.navigateByUrl('/user-type',{replaceUrl:true});
   }
 
 }
