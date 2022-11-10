504703ab822527b6f971f5180164303a6f9b6fc1
Sketchead <megamam_atxel@hotmail.com>
2022-11-09 20:03:58 -0700

    Add back button view post

--- src/app/view-post/view-post.page.ts
+++ src/app/view-post/view-post.page.ts
@@ -5,7 +5,8 @@ import { ActivatedRoute } from '@angular/router';
 import { Firestore, collection, collectionData, docData, addDoc, deleteDoc, updateDoc,query, getDoc, doc, getFirestore } from '@angular/fire/firestore';
 import { DataService, Post } from '../services/data.service';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
-
+import { AuthService } from '../services/auth.service';
+import { Auth } from '@angular/fire/auth';
 @Component({
   selector: 'app-view-post',
   templateUrl: './view-post.page.html',
@@ -15,24 +16,27 @@ export class ViewPostPage implements OnInit {
 
   public post : Post = {title:'a',text:'e',positive:true};
   public a: Params;
-  constructor(private dataservice: DataService,private route: ActivatedRoute,private firestore: Firestore) {
+  public isOwner: Boolean;
+  constructor(private dataservice: DataService,private route: ActivatedRoute,private firestore: Firestore, private auth: Auth) {
    }
 
   async ngOnInit() {
     this.route.queryParams.subscribe(async (params) => {
-      console.log(params.id);
+      //console.log(params.id);
       const docRef = doc(this.firestore,`posts/${params.id}`)
-      let dataDoc = null
       await getDoc(docRef)
         .then(async (doc)=>{
           console.log(doc.data(),doc.id)
-          dataDoc = doc.data()
           this.post.title = await doc.data().title
           this.post.images = await doc.data().images
           this.post.text = await doc.data().text
           this.post.author = await doc.data().author
           this.post.positive = await doc.data().positive
           this.post.pos = await doc.data().pos
+
+          if(this.post.author === this.auth.currentUser.uid){
+            this.isOwner=true;
+          }
         })
     });    
   }
