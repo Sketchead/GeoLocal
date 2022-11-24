e4123eba664b23b373c4ed95393095d78aff9796
heleperezcr <heleperezcr@ittepic.edu.mx>
2022-11-23 18:24:53 -0600

    puntos en el mapa

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,6 +1,8 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { GoogleMap } from '@capacitor/google-maps';
+import { tick } from '@angular/core/testing';
+import { GoogleMap, Marker } from '@capacitor/google-maps';
 import { LoadingController } from '@ionic/angular';
+import { title } from 'process';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -41,10 +43,32 @@ export class MapPage implements OnInit {
         minZoom: 5
       }
     });
+    this.addMarkers();
     await loading.dismiss()
   }
 
   //metodo puntos
+  async addMarkers(){
+    const markers: Marker[] = [
+      {
+        coordinate: {
+          lat: 21.5095,
+          lng: -104.8957,
+        },
+        title: 'localhost',
+        snippet:'Best place on earth',
+      },
+      {
+        coordinate: {
+          lat: 21.5090,
+          lng: -104.8950,
+        },
+        title: 'random place',
+        snippet:'Not sure',
+      },
+    ];  
+    await this.map.addMarkers(markers);
+  }
 
 
 }
